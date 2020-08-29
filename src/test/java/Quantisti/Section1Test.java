cc927aeba9b6c8af61debad8349d4298e85e3f4c
Rohit <rohitmore648@gmail.com>
2020-08-30 00:08:18 +0530

    third

--- src/test/java/Quantisti/Section1Test.java
+++ src/test/java/Quantisti/Section1Test.java
@@ -14,7 +14,7 @@ import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
-public class Section1 {
+public class Section1Test {
 
 	public void RemindMeLater(WebDriver driver) {
 
